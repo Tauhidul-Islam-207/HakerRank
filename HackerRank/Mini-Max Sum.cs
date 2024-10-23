8624ed88f4c13a1d06867ec39527b7c722ce273a
Tauhidul Islam <tauhidulislam207@gmail.com>
2024-10-23 21:49:11 +0600

    Create Mini-Max Sum.cs

---
+++ HackerRank/Mini-Max Sum.cs
@@ -0,0 +1,46 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<int> arr)
+    {
+        arr.Sort();
+        long sum = 0;
+        foreach(int i in arr){
+            sum += i;
+        }
+        
+        Console.WriteLine($"{sum-arr[4]} {sum-arr[0]}");
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.miniMaxSum(arr);
+    }
+}
